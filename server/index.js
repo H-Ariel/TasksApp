b06dac228f0072ec7db1f195397683625edc7108
ariel halili <arielhalili259@gmail.com>
2024-06-17 14:45:46 +0300

    reformat code

--- server/index.js
+++ server/index.js
@@ -5,66 +5,54 @@ const port = 3001;
 const app = express();
 const db = new MyDB.MySqliteDB();
 
+
 // Middleware
 app.use(express.json());
 
+
 // Define routes
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('This is the root route. Please use /api/tasks to access the tasks');
     //res.redirect("http://localhost:3000");
 });
 
-app.get('/api/data', (req, res) => {
-    res.json({ message: 'Hello from server!' });
-});
 
 // Define CRUD operations
 // Create a new task
-app.post('/api/tasks', async (req, res) => {
-    try {
-        const task = await db.addTask(new MyDB.TaskObject(req.body.text));
-        res.status(201).json(task);
-    } catch (err) {
-        res.status(400).json({ error: 'Failed to create Task' });
-    }
-});
+app.post('/api/tasks', async (req, res) =>
+    db.addTask(new MyDB.TaskObject(req.body.text))
+        .then((task) => res.status(201).json(task))
+        .catch((err) => res.status(400).json({ error: 'Failed to create Task' }))
+);
 
 // Read all Tasks
-app.get('/api/tasks', async (req, res) => {
-    try {
-        const tasks = await db.getTasks();
-        res.json(tasks);
-    } catch (err) {
-        res.status(500).json({ error: 'Failed to retrieve Tasks' });
-    }
-});
+app.get('/api/tasks', async (req, res) =>
+    db.getTasks()
+        .then((tasks) => res.json(tasks))
+        .catch((err) => res.status(500).json({ error: 'Failed to retrieve Tasks' }))
+);
 
 // Update a Task
 app.put('/api/tasks/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const task = await db.getTask(id);
-        if (req.body.hasOwnProperty('text'))
-            task.text = req.body.text;
-        if (req.body.hasOwnProperty('completed'))
-            task.completed = req.body.completed;
-        await db.updateTask(id, task);
-        res.json(task);
-    } catch (err) {
-        res.status(400).json({ error: 'Failed to update Task' });
-    }
+    const { id } = req.params;
+    db.getTask(id)
+        .then((task) => {
+            if (req.body.hasOwnProperty('text'))
+                task.text = req.body.text;
+            if (req.body.hasOwnProperty('completed'))
+                task.completed = req.body.completed;
+
+            return db.updateTask(id, task);
+        }).then((task) => res.json(task))
+        .catch((err) => res.status(400).json({ error: 'Failed to update Task' }))
 });
 
 // Delete a Task
-app.delete('/api/tasks/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        await db.deleteTask(id);
-        res.status(204).send();
-    } catch (err) {
-        res.status(400).json({ error: 'Failed to delete Task' });
-    }
-});
+app.delete('/api/tasks/:id', async (req, res) =>
+    db.deleteTask(req.params.id)
+        .then(() => res.status(204).send())
+        .catch((err) => res.status(400).json({ error: 'Failed to delete Task' }))
+);
 
 
 // Start the server
